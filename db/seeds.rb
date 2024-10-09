552b07bd7cf3de94bb387439f31c7062b443fa1f
Frank Condezo <frankcondezo@gmail.com>
2024-10-08 23:43:53 -0500

    add authentification

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# create a user
+
+User.create!(
+  email_address: "admin@gmail.com",
+  password: "letmein1"
+)
